57277a661bb93fdd04d62856a727239497fca072
bart <bart@trafficdigital.com>
2013-02-07 17:39:25 +0000

    fetching cords

--- www/js/index.js
+++ www/js/index.js
@@ -29,17 +29,22 @@ var app = {
         app.geolocation();
     },
     geolocation: function(){
-        navigator.geolocation.getCurrentPosition(app.getCords(position), onError);
-    },
-    getCords: function(position) {
-        var element = document.getElementById('geolocation');
-        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            'Longitude: '          + position.coords.longitude             + '<br />' +
-                            'Altitude: '           + position.coords.altitude              + '<br />' +
-                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                            'Heading: '            + position.coords.heading               + '<br />' +
-                            'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          + position.timestamp                    + '<br />';
-    }
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }       
+};
+
+
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
+var onError = function() {
+    alert('onError!');
 };
